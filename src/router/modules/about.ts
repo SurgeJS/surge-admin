e452621d5d3601cb9c71a9413e855d3d4ec6ca2f
tudo <419626398@qq.com>
2024-08-12 15:21:39 +0800

    fix：修复一些bug

--- src/router/modules/about.ts
+++ src/router/modules/about.ts
@@ -2,8 +2,8 @@ export default {
   path: '/about',
   meta: {
     title: '关于',
-    keepAlive:true,
     icon: 'ant-design:like-filled',
+    order: 5,
   },
   component: 'menu'
 } satisfies AppRouteRecordRaw
