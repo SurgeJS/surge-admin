de50ff5d2fce64934ebb70f7e743060218038c12
tudo <419626398@qq.com>
2024-09-19 17:55:31 +0800

    fix:tabBar

---
+++ src/views/auth/components/DynamicBackground.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dynamicBackground" />
+</template>
+
+<style scoped lang="scss">
+.dynamicBackground {
+  height: 100%;
+  width: 100%;
+  background: theme('colors.primary');
+  -webkit-mask: url('@/assets/svg/bg.svg') no-repeat bottom;
+  mask: url('@/assets/svg/bg.svg') no-repeat bottom;
+  background-size: contain;
+  position: absolute;
+  z-index: 0;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
---
+++ src/views/auth/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-h-full flex flex-center relative">
+    <dynamic-background />
+    <div class="absolute right-[10px] top-[10px] bg-layer p-[5px] rounded-sm flex gap-[5px]">
+      <theme-palette />
+      <theme-switch />
+    </div>
+    <n-card
+      v-motion-pop-visible
+      class="auth-card lg:w-[55vh] md:w-[60%] w-[70%] h-[60vh] transition-width"
+      content-class="h-full"
+    >
+      <div class="w-h-full flex overflow-hidden">
+        <div class="h-full flex-1 flex-shrink-0 p-[10px]">
+          <router-view v-slot="{ Component, route }">
+            <component
+              :is="Component"
+              :key="route.fullPath"
+              v-motion-slide-right
+            />
+          </router-view>
+        </div>
+      </div>
+    </n-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import DynamicBackground from '@/views/auth/components/DynamicBackground.vue'
+</script>
+
+<style lang="scss" scoped>
+.auth-card {
+  box-shadow: 0 0 40px -15px theme('colors.primary-shallow');
+}
+</style>
+
